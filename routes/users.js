f80267f620fb3b291bcb62fb504d9a6d64de67f7
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-02-14 13:50:39 -0300

    feat: patch route

--- routes/users.js
+++ routes/users.js
@@ -31,4 +31,18 @@ router.delete("/:id", (req, res) => {
   res.send(`User with the id: ${id} deleted from database.`);
 });
 
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+  const user = users.find((user) => user.id === id);
+
+  if (firstName) user.firstName = firstName;
+
+  if (lastName) user.lastName = lastName;
+
+  if (age) user.age = age;
+
+  res.send(`User with the id: ${id} updated.`);
+});
+
 export default router;
